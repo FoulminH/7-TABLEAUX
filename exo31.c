fde99162d4881859740a8fec4459f5908f64151b
FoulminH <45918723+FoulminH@users.noreply.github.com>
2022-05-13 12:06:07 +0200

    Update exo31.c

--- exo31.c
+++ exo31.c
@@ -8,12 +8,29 @@ int recherche(int valeur, int tableau[], int taille){
     }return i;
 };
 
+int rechercheDicho(int valeur,int tableau[], int taille){
+    int start = 0;
+    int end = taille;
+    int i;
+    while(start<end){
+        i = (start+end)/2;
+        if (valeur < tableau[i]){
+            end = i;
+        }else if(valeur > tableau[i]){
+            start = i + 1;
+        }else{
+            return i;
+        }
+    }
+};
+
 int main()
 {
     int valeur = 2;
     int tableau[] = {1,2,3,4,5,6};
     int taille = sizeof tableau / sizeof tableau[0];
     printf("La valeur cherchée se trouve à l'indice %d du tableau",recherche(valeur,tableau,taille));
+    printf("A l'aide de la recherche dichotomique : la valeur cherchée se trouve à l'indice %d du tableau",rechercheDicho(valeur,tableau,taille));
 
     return 0;
 }
