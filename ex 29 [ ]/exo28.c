7f86023e672858e52bf750e5d8c1c5050715494a
FoulminH <45918723+FoulminH@users.noreply.github.com>
2022-05-17 18:54:30 +0200

    exo29.c

--- ex 29 [ ]/exo28.c
+++ ex 29 [ ]/exo28.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <curses.h>
-
+#include <ncurses.h>
 #define HAUTEUR 9
 #define LARGEUR 33
 /* is_valid vérifie que les coordonnées (x, y) sont valides
 ,→ pour un déplacement */
 int is_valid(int x, int y, int hauteur, int largeur, char grille[hauteur][largeur]) {
-return 1;
+    int value;
+    if(x >= 0 && x < largeur && y >= 0 && y < hauteur){
+        if(grille[y][x]=='#'){
+            value= 0;
+        }else{
+            value= 1;
+        }
+    }
+    return value;
 }
 /* is_finish vérifie que l'emplacement sur lequel se trouve
 ,→ les coordonnées (x, y) est une sortie */
 int is_finish(int x, int y, int hauteur, int largeur, char grille[hauteur][largeur]) {
-return 0;
+if(grille[y][x]=='.'){
+    return 1;
+}else{
+    return 0;    
+}
+
 }
 int main() {
 char grille[HAUTEUR][LARGEUR] = {
@@ -25,6 +37,7 @@ char grille[HAUTEUR][LARGEUR] = {
 "# ## # ########### ###### ######",
 "#    #              #          #",
 "##############################.#"};
+
 int x = 1, y = 0;
 int move_x, move_y;
 int i;
@@ -46,7 +59,7 @@ case 's': move_y = y + 1; break;
 case 'q': move_x = x - 1; break;
 case 'd': move_x = x + 1; break;
 }
-if(is_valid(move_x, move_y, HAUTEUR, LARGEUR,grille)) {
+if(is_valid(move_x, move_y, HAUTEUR, LARGEUR, grille)) {
 x = move_x; y = move_y;
 }
 } while(! is_finish(x, y, HAUTEUR, LARGEUR, grille));
